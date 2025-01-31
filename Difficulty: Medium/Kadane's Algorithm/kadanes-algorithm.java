4661cd19468083fdb8fec8b41850fce2d195e625
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2025-01-31 19:35:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        // int r=arr[0];
+        // for(int i=0;i<arr.length;i++){
+        //     int temp=0;
+        //     for(int j=i;j<arr.length;j++){
+        //         temp+=arr[j];
+        //         r=Math.max(r,temp);
+        //     }
+        // }
+        // return r;
+        int a=Integer.MIN_VALUE;
+        int s=0;
+        for(int i=0;i<arr.length;i++){
+            s+=arr[i];
+            a=Math.max(a,s);
+            if(s<0){
+                s=0;
+            }
+        }
+        return a;
+    }
+}
