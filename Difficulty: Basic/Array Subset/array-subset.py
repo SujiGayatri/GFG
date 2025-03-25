853007f7a4381f92a2e7528c40bafc0c9a2e0b6d
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2025-03-25 16:33:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.py
@@ -0,0 +1,47 @@
+#User function Template for python3
+from collections import Counter
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        # Your code here
+        freq_a = Counter(a)  
+    
+        for num in b:
+            if freq_a[num] > 0:
+                freq_a[num] -= 1  
+            else:
+                return False  
+        return True
+    
+    
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while (T > 0):
+        a1 = [int(x) for x in input().strip().split()]
+        a2 = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        if ob.isSubset(a1, a2):
+            print("true")
+        else:
+            print("false")
+
+        T -= 1
+
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
