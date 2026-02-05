39e74e18bb80ef2dec3f72a387d17544cc11b3ed
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-02-05 21:43:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 1/pattern-1.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+class Solution:
+    def printSquare(self, N):
+        # Code here
+        for i in range(N):
+            for j in range(N):
+                print("*",end=" ")
+            print()
