1019e6e1dd3952f7340306f8c072db21d893135c
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-03-05 21:51:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Missing number in shuffled array/missing-number-in-shuffled-array.py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+
+class Solution:
+    def findMissing(self, arr1,arr2):
+        # code here
+        sum1 = sum(arr1)
+        sum2 = sum(arr2)
+        return sum1-sum2
