12233ad461c9f2f72799741f93202010502a0cf1
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-03-12 22:31:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reversing the vowels/reversing-the-vowels.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    def modify(self, s):
+        # code here
+        vowels = set('aeiou')
+        s = list(s)
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and s[left] not in vowels:
+                left += 1
+            while left < right and s[right] not in vowels:
+                right -= 1
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        return "".join(s)
