7e26c2d6dca305691e8dd664fceb3ff9857e5927
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-05-14 22:22:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Frequency/find-the-frequency.py
@@ -0,0 +1,12 @@
+"""
+You're given an array (arr)
+Return the frequency of element x in the given array
+"""
+class Solution:
+    def findFrequency(self, arr, x):
+        # code here
+        count = 0
+        for num in arr:
+            if num == x:
+                count += 1
+        return count
