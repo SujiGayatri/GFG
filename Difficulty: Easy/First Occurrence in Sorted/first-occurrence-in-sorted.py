b946d5abd9f4d421faf2b9c3ec90890e2931400a
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-08-16 23:15:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstSearch(self, arr, k):
+        # Code Here
+        low, high = 0, len(arr) - 1
+        ans = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == k:
+                ans = mid
+                high = mid - 1     
+            elif arr[mid] < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return ans
